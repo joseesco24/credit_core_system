bd33d35daa4f1fc5e81a8e245f4aa79403d06350
jose.murcia <jose.murcia@rappi.com>
2025-06-19 21:54:45 -0500

    feat: remove cache from authentication client

--- src/modules/user/rest_clients/user_authentication_client.py
+++ src/modules/user/rest_clients/user_authentication_client.py
@@ -3,7 +3,6 @@ from typing import Self
 from urllib.parse import urljoin
 
 import httpx
-from cachetools import TTLCache
 from fastapi import HTTPException
 from fastapi import status
 from httpx import Response
@@ -13,17 +12,12 @@ from src.sidecard.system.artifacts.env_provider import EnvProvider
 
 __all__: list[str] = ["UserAuthenticationClient"]
 
-user_authentication_client_cache: TTLCache = TTLCache(ttl=10, maxsize=20)
-
 
 class UserAuthenticationClient:
     def __init__(self: Self):
         self._env_provider: EnvProvider = EnvProvider()  # type: ignore
         self.base_url: str = str(self._env_provider.identity_validation_ms_base_url)
         self._httpx_client: httpx.AsyncClient = httpx.AsyncClient()
-
-    def clear_cache(self: Self) -> None:
-        user_authentication_client_cache.clear()
 
     @retry(on=HTTPException, attempts=8, wait_initial=0.4, wait_exp_base=2)
     async def obtain_user_autentication(self: Self) -> bool:
